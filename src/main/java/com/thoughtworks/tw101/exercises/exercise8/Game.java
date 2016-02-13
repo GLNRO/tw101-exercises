173c7ed4a3698c090a7ba7e3225e49635f430df8
GLNRO <lg@glnro.com>
2016-02-13 12:56:59 -0500

    Write parseGuess method

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/Game.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/Game.java
@@ -6,11 +6,19 @@ import java.util.Scanner;
 /**
  * Created by laurengallinaro on 2/13/16.
  */
+// When I instantiate the game, I want to initialize a random number, array of guesses, and new scanner reader
+// When I start the game, I want to ask a user for their guess
+// When the user inputs their guess, I want to parse that input to an integer
+// When the user inputs their guess, I want to catch any numformat exceptions and ask them to try again.
+//
+//
+
 public class Game {
 
     private Integer randNum;
     private ArrayList guesses = new ArrayList<Integer>();
     private Scanner scanner = new Scanner(System.in);
+    private int userGuess;
 
     public Game(){
         this.randNum = (int)Math.random()*5;
@@ -19,11 +27,26 @@ public class Game {
     public void start(){
         System.out.println("Starting the game");
         getInput();
-
+        System.out.println("You guessed " + userGuess + " but you should have have guessed " + randNum);
     }
 
+
+
     public void getInput(){
-        String userGuess = scanner.nextLine();
-        System.out.println(userGuess);
+        System.out.println("Pick a random number...");
+        String stringGuess = scanner.nextLine();
+        parseGuess(stringGuess);
+    }
+
+    public void parseGuess(String input){
+
+        try{
+            userGuess = Integer.parseInt(input);
+        }
+        catch(NumberFormatException e){
+            System.out.println("That's not a number, try again...");
+            getInput();
+        }
+
     }
 }
