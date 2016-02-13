8e90d72b8700392ec5fab3c1968a4685730cab8c
GLNRO <lg@glnro.com>
2016-02-13 12:28:42 -0500

    Write getInput method

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/Game.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/Game.java
@@ -1,14 +1,16 @@
 package com.thoughtworks.tw101.exercises.exercise8;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by laurengallinaro on 2/13/16.
  */
 public class Game {
 
-    private int randNum;
-    private ArrayList<int> guesses = new ArrayList<int>();
+    private Integer randNum;
+    private ArrayList guesses = new ArrayList<Integer>();
+    private Scanner scanner = new Scanner(System.in);
 
     public Game(){
         this.randNum = (int)Math.random()*5;
@@ -16,5 +18,12 @@ public class Game {
 
     public void start(){
         System.out.println("Starting the game");
+        getInput();
+
+    }
+
+    public void getInput(){
+        String userGuess = scanner.nextLine();
+        System.out.println(userGuess);
     }
 }
