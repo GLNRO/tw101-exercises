e4e580bee893cd1e5cb9a40cb1334ca20ed27ec8
GLNRO <lg@glnro.com>
2016-02-17 00:13:30 -0500

    wip

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -2,14 +2,33 @@ package com.thoughtworks.tw101.exercises.exercise9;
 
 import java.util.List;
 
+// If a new node comes before
+
 public class Node {
-    private String name;
+    public String name;
+    private Node leftChild;
+    private Node rightChild;
 
     public Node(String name) {
         this.name = name;
     }
 
     public void add(String nameOfNewNode) {
+        if (nameOfNewNode.compareTo(this.name) < 0 ){
+            if (leftChild == null) {
+                leftChild = new Node(nameOfNewNode);
+            }
+            else{
+                leftChild.add(nameOfNewNode)
+            }
+        }
+        else if(nameOfNewNode.compareTo(this.name) > 0){
+
+        }
+        else if(nameOfNewNode.compareTo(this.name) == 0){
+
+        }
+
     }
 
     public List<String> names() {
