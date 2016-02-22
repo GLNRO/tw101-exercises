d7838bc4a986cd412ea380ccfb30100abb548bae
GLNRO <lg@glnro.com>
2016-02-21 19:22:55 -0500

    Write diamond methods

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -1,9 +1,11 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
+import java.util.*;
+
 public class DiamondExercises {
     public static void main(String[] args) {
         drawAnIsoscelesTriangle(3);
-        drawADiamond(8);
+        drawADiamond(3);
         drawADiamondWithYourName(3);
     }
 
@@ -13,8 +15,22 @@ public class DiamondExercises {
 //             ***
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
+        for (int i=0;i<n;i++){
+            String spaces = repeat(n-(i+1)," ");
+            String stars = repeat((2*i)+1, "*");
+            System.out.println(spaces + stars);
+        }
 
     }
+
+    private static String repeat(int n, String character){
+        String returnItem = "";
+        for(int i=0;i<n;i++){
+            returnItem += character;
+        }
+        return returnItem;
+    }
+
 
 //    Diamond
 //    Given a number n, print a centered diamond. Example for n=3:
@@ -24,7 +40,16 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
-
+        for (int i=0;i<n;i++){
+            String spaces = repeat(n-(i+1)," ");
+            String stars = repeat((2*i)+1, "*");
+            System.out.println(spaces + stars);
+        }
+        for (int i =(n-2);i>=0;i--){
+            String spaces = repeat(n-(i+1)," ");
+            String stars = repeat((2*i)+1, "*");
+            System.out.println(spaces + stars);
+        }
     }
 
 //    Diamond with Name
@@ -36,6 +61,16 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
-
+        for (int i=0;i<n;i++){
+            String spaces = repeat(n-(i)," ");
+            String stars = repeat((2*i)+1, "*");
+            System.out.println(spaces + stars);
+        }
+        System.out.println("Lauren");
+        for (int i =(n-1);i>=0;i--){
+            String spaces = repeat(n-(i)," ");
+            String stars = repeat((2*i)+1, "*");
+            System.out.println(spaces + stars);
+        }
     }
 }
